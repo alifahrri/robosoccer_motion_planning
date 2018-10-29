504c44a4e7762efa83e3eb28c55f9f4539d7acdb
Fahri Ali Rahman <ali.rahman.fahri@gmail.com>
2018-10-29 10:52:20 +0700

    join the dark side!

--- src/robosoccer_interface/robosoccer_teleop/scripts/robosoccer_teleop_node.py
+++ src/robosoccer_interface/robosoccer_teleop/scripts/robosoccer_teleop_node.py
@@ -3,10 +3,15 @@ import gui.teleopgui as gui
 import rospy
 import sys
 
+import qdarkstyle
+modulename = 'qdarkstyle'
+
 from PyQt5 import QtWidgets, QtGui, QtCore
 
 if __name__ == '__main__' :
     app = QtWidgets.QApplication(sys.argv)
+    if modulename in sys.modules:
+        app.setStyleSheet(qdarkstyle.load_stylesheet_pyqt5())
     teleop = gui.TeleopGUI()
     rospy.init_node('robosoccer_teleop')
     teleop.showMaximized()
