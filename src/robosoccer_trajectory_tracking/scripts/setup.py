dddd03921f7d2706edd322f72acbf2ded1836fab
Fahri Ali Rahman <ali.rahman.fahri@gmail.com>
2018-10-31 10:42:52 +0700

    minor change

--- src/robosoccer_trajectory_tracking/scripts/setup.py
+++ src/robosoccer_trajectory_tracking/scripts/setup.py
@@ -1,6 +1,17 @@
+#!/usr/bin/cython
 from distutils.core import setup
 from Cython.Build import cythonize
+from distutils.extension import Extension
+from Cython.Distutils import build_ext
 
 setup(
-    ext_modules = cythonize("*.pyx")
+    name = 'tracker',
+    ext_modules=[
+    Extension('test',
+            sources=['tracker.pyx'],
+            extra_compile_args=['-O3'],
+            language='c++')
+    ],
+    cmdclass = {'build_ext': build_ext}
+    # ext_modules = cythonize("*.pyx"),
 )
